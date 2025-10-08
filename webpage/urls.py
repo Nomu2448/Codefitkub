1f9945f74dd152bf509a8ebfe2dd36bcd629a2fc
bXiilzy <jiranankredkla12@gmail.com>
2025-10-08 15:31:52 +0700

    fix ui login-regit

--- webpage/urls.py
+++ webpage/urls.py
@@ -3,11 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('',  views.login_view, name='login'),
+    path('home/', views.home, name='home'),
     path('bmi', views.bmi, name='bmi'),
     path('nutrition/', views.nutrition, name='nutrition'),
     path('profile/', views.profile, name='profile'),
-    path('login/', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
     path('logout/', views.logout_view, name='logout'),
 ]
